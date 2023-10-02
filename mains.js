1e21778dae393646c5a29a5a4c749b8e1006daa3
sevenmaluco7070 <129309165+sevenmaluco7070@users.noreply.github.com>
2023-10-02 12:21:39 -0300

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,48 @@
+alert('Ola Mundo');
+function tocaSomPom(){
+document.querySelector('#som_tecla_pom').play();
+ document.querySelector('.tecla_pom').onclick=tocaSomPom;}
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+    document.querySelector('.tecla_clap').onclick=tocaSomClap;}
+
+ function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+     document.querySelector('.tecla_tim').onclick=tocaSomTim;}
+
+ function tocaSomPuff(){
+     document.querySelector('#som_tecla_puff').play();
+      document.querySelector('.tecla_puff').onclick=tocaSomPuff;}
+
+ function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+     document.querySelector('.tecla_splash').onclick=tocaSomSplash;}
+     
+
+function tocaSomToim(){
+   document.querySelector('#som_tecla_toim').play();
+    document.querySelector('.tecla_toim').onclick=tocaSomToim;}
+
+ function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+     document.querySelector('.tecla_psh').onclick=tocaSomPsh;}
+     
+
+    function tocaSomTic(){
+        document.querySelector('#som_tecla_tic').play();
+        document.querySelector('.tecla_tic').onclick=tocaSomtic;}
+
+        function tocaSomTom(){
+            document.querySelector('#som_tecla_tom').play();
+        document.querySelector('.tecla_tom').onclick=tocaSomTom;}
+
+        const listaDeTeclas = document.querySelectorA11('.tecla');
+
+      let contador = 0;
+        listaDeTeclado[0].onclick = tocaSomPom;    
+
+        while(contador <listaDeTeclas.length) {
+            listaDeTeclas[contador].onclick = tocaSomPom;
+            contador = contador + 1;
+        }    
